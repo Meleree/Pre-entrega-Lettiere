a63f50274342be5b59558c4aefada16e643bca45
Meleree <melisa.gis@gmail.com>
2024-11-28 19:47:49 -0300

    segundo commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,22 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';  
-import melereImage from '../assets/melere.webp';  
-import CartWidget from './CartWidget';  
-import './navbar.css';  
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from './CartWidget';
 
-const NavBar = () => {
+function NavBar({ bg, theme }) {
   return (
-    <div className="navbar-container">
-      {/* Imagen del logo */}
-      <img src={melereImage} alt="Melere" className="navbar-logo" />
-
-      {/* Barra de navegación */}
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Nav className="mx-auto">
-            <Nav.Link href="#abrigos">Abrigos</Nav.Link>
-            <Nav.Link href="#remeras">Remeras</Nav.Link>
-            <Nav.Link href="#accesorios">Accesorios</Nav.Link>
-          </Nav>
-
-          <div className="navbar-cart">
-            <CartWidget />
-          </div>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar bg={bg} data-bs-theme={theme}>
+      <Container>
+        <img src="../src/assets/melere.webp" alt="Logo" />
+        <Nav className="me-auto">
+          <Nav.Link href="#abrigos">Abrigos</Nav.Link>
+          <Nav.Link href="#remeras">Remeras</Nav.Link>
+          <Nav.Link href="#accesorios">Accesorios</Nav.Link>
+          <CartWidget />
+        </Nav>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default NavBar;
